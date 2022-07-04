c935d54fe7dc1857b82fb5194563d5df95ccbb1a
Sebastien Stordeur <stordeursebastien0@gmail.com>
2022-07-04 10:55:12 +0200

    404 page responsive

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -6,7 +6,7 @@ const Page404 = () => {
     <section className="section-404">
       <h1 className="title-404">404</h1>
       <p className="p-404">Oups! La page que vous demandez n'existe pas.</p>
-      <Link to="/" className="a-404">Retourner sur la page d'acceuil</Link>
+      <Link to="/" className="a-404">Retourner sur la page d'accueil</Link>
     </section>
   )
 }
